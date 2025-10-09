a825cf24a57488d0cc7734746e5160fe5aa4b9fd
isaac <ishak.hadj14@gmail.com>
2025-10-09 21:29:17 +0100

    feat(Contact Section): add the contact section

---
+++ src/components/organism/Contact/Contact.tsx
@@ -0,0 +1,23 @@
+import SectionWrapper from "@/components/common/SectionWrapper";
+import ContactCard from "./ContactCard";
+import ContactForm from "./ContactForm";
+import SectionHeader from "@/components/common/SectionHeader";
+import socialAccounts from "./SocialLinks";
+export default function Contact() {
+  return (
+    <SectionWrapper sectionName={"Contact"}>
+      <SectionHeader title={"Get In Touch"} subTitle={"Contact Me"} />
+      <div className="grid grid-cols-1 md:grid-cols-2  justify-between lg:pt-10 gap-y-16 md:gap-y-0  md:gap-x-6">
+        <div className="reach-out flex  flex-col items-center gap-y-5 ">
+          <h2 className="text-subTitle dark:text-zinc-100/80 font-bold text-xl md:text-2xl  mb-3">
+            Reach Out
+          </h2>
+          {socialAccounts.map((socialItem) => {
+            return <ContactCard key={socialItem.icon} socialObj={socialItem} />;
+          })}
+        </div>
+        <ContactForm />
+      </div>
+    </SectionWrapper>
+  );
+}
