62f8dac841fb723e3c763f75f3d0ef73bf3ce4cc
isaac <ishak.hadj14@gmail.com>
2025-10-09 17:54:11 +0100

    Add the portfolio section

---
+++ src/components/organism/Portfolio/Portfolio.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import projectsData from "./ProjectsData";
+import ProjectCard from "./ProjectCard";
+import SectionWrapper from "@/components/common/SectionWrapper";
+import SectionHeader from "@/components/common/SectionHeader";
+
+export default function Portfolio() {
+  //I will implement this later, ignore it
+  const [showMore, setShowMore] = useState(false);
+  const finalProjectsArr = showMore ? projectsData : projectsData.slice(0, 6);
+  return (
+    <SectionWrapper sectionName={"Portfolio"}>
+      <SectionHeader title={"Portfolio"} subTitle={"My Work"} />
+
+      <ul className="grid grid-cols-[repeat(1,_minmax(0,_300px))]   sm:grid-cols-2 xl:grid-cols-[repeat(3,_minmax(0,_450px))] gap-5 justify-center">
+        {finalProjectsArr.map((projectItem) => {
+          return <ProjectCard key={projectItem.name} project={projectItem} />;
+        })}
+      </ul>
+      <button
+        className="main-btn !block !mx-auto mt-8 cursor-target"
+        onClick={() => setShowMore((prev) => !prev)}
+      >
+        {showMore ? "Show Less" : "Show More"}
+      </button>
+    </SectionWrapper>
+  );
+}
