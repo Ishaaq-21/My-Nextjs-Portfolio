3d9781cd11e98f99180ad90712abed84f3283be4
isaac <ishak.hadj14@gmail.com>
2025-10-10 00:01:49 +0100

    refactor: add the necessary content to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+import TargetCursorWrapper from "@/components/AnimatedUiBlocks/TargetCursor";
+import ScrollBar from "@/components/atoms/ScrollBar";
+import ScrollTopTopOnReload from "@/components/atoms/ScrollToTopOnReload";
+import MainSections from "@/components/organism/MainSections/MainSections";
+import MobileScreenContextProvider from "@/contexts/MobileScreenContext";
 
 export default function Home() {
   return (
+    <MobileScreenContextProvider>
+      <TargetCursorWrapper />
+      <ScrollTopTopOnReload />
+      <ScrollBar />
+      <MainSections />
+    </MobileScreenContextProvider>
   );
 }
